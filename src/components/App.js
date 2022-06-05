2d77954ee3874136c0a3ef90594b6bf0a4504063
Kevin-DeBruyne <70618739+Kevin-DeBruyne@users.noreply.github.com>
2022-06-05 22:17:53 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import Notes from "../notes";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {Notes.map((notes) => (
+        <Note key={notes.id} title={notes.title} content={notes.content} />
+      ))}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
